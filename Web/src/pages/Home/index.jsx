e34c5ab430138fcfac6957bd04d207899501e0b4
Tiamo <jiang_wzj@163.com>
2025-01-16 17:40:57 +0800

    Web project add Route config

---
+++ Web/src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+// src/pages/Home/index.jsx
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="home-content">
+                <div className="home-content-title">欢迎使用 MiGPT</div>
+                <div className="home-content-subtitle">基于 React 的 MiGPT 可视化配置界面，提供直观的服务管理和参数配置能力。</div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
---
+++ Web/src/router/index.jsx
@@ -0,0 +1,12 @@
+// src/router/index.jsx
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+ 
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element: <Home />,
+    }
+])
+ 
+export default router
