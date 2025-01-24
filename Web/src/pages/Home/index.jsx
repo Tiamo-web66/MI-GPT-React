c51171771471a75b0e1e9052f16f8699d8af060d
Tiamo <jiang_wzj@163.com>
2025-01-24 16:06:29 +0800

    Home HTML dev

--- Web/src/pages/Home/index.jsx
+++ Web/src/pages/Home/index.jsx
@@ -1,11 +1,16 @@
 // src/pages/Home/index.jsx
 import React from 'react';
-import Header from './components/Header'
+import Header from './components/Header';
+import './index.less';
+import { ControlOutlined } from '@ant-design/icons';
 const Home = () => {
     return (
         <>
-        <Header/>
         <div className="home">
+            <div className='header'>
+                <div className='headerLeft'>Ti - GPT</div>
+                <div className='headerRight'><a href=""><ControlOutlined style={{marginRight: '6px'}} />配置中心</a></div>
+            </div>
             <div className="home-content">
                 <div className="home-content-title">欢迎使用 MiGPT</div>
                 <div className="home-content-subtitle">基于 React 的 MiGPT 可视化配置界面，提供直观的服务管理和参数配置能力。</div>
